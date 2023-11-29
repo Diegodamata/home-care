fddd8c44b2ac2a3d4a96de6c6723b088684760ca
Diego da Mata <Diegodamata>
2023-11-29 20:23:34 -0300

    validei o modal em javascript

--- html/principal.php
+++ html/principal.php
@@ -132,6 +132,21 @@
             font-size: 20px;
         }
         
+        .input p{
+            color: #ff3b25;
+            margin-top: -10px;
+            margin-left: 7px;
+            visibility: hidden;
+        }
+        /*erros*/
+        .input.erro input{
+            border-color: #ff3b25;
+        }
+        /*ir aparecer a msg de erro*/
+        .input.erro p{
+            color: #ff3b25;
+            visibility: visible;
+        }
 
     </style>
 </head>
@@ -162,15 +177,19 @@
                                 <option >Psicologo</option>
                                 <option >Bucomaxilo</option>
                             </datalist>
+                            <p class="erro">msg erro</p>
                         </div>
                         <div class="input">
                             <input type="text" name="nome" id="nome" placeholder="Nome:">
+                            <p class="erro">msg erro</p>
                         </div>
                         <div class="input">
                             <input type="email" name="email" id="email" placeholder="Email:">
+                            <p class="erro">msg erro</p>
                         </div>
                         <div class="input">
                             <input type="cell" name="fone" id="fone" placeholder="Telefone:">
+                            <p class="erro">msg erro</p>
                         </div>
                         <div class="input">
                             <input type="file" name="arquivo" id="arquivo">
@@ -356,7 +375,107 @@
                                                             //se se existir a classe hide ira remover e se nao existir ira add
         })
 
-        
+        const form = document.querySelector(".formulario");
+        const areaAtuacao = document.querySelector("#prof");
+        const nome = document.querySelector("#nome");
+        const emailvalor = document.querySelector("#email");
+        const telefone = document.querySelector("#fone");
+
+
+
+        form.addEventListener('submit', (event)=>{ //evento de submit no formulário
+            event.preventDefault(); //função para evitar do formulario ser enviado sem os preenchimentos
+            
+            verificarChecagem();
+
+        })
+
+        areaAtuacao.addEventListener("input", () => { //evento para quando o usuario depois de ter aparecido o erro, quando ele voltar a digitar sumir o erro
+            checarAtuacao();
+        })
+
+        nome.addEventListener("input", () => {
+            checarNome();
+        })
+
+        emailvalor.addEventListener("input", () => {
+            checarEmail();
+        })
+
+        telefone.addEventListener("input", () => {
+            checarTelefone();
+        })
+
+
+
+        function checarAtuacao() { //função para checar o nome se esta vazio
+            const nameValue = areaAtuacao.value; //adicionei o valor do input a uma variavel pegando o valor desse input
+
+            if(nameValue === ""){ //se vazio
+                errorInput(areaAtuacao, "Informe a área de atuação.") //chama a classe errorIput e passa dois parametros o proprio input ea msg de erro que eu quero que aparece
+            }else{
+                const formItem = areaAtuacao.parentElement; // se as duas condiçoes de cima for falsa o usuario preencheu corretamente
+                formItem.className = "input"; //e entao chama a classe imput sem o erro
+            }
+        }
+
+        function checarNome() { //função para checar o nome se esta vazio
+            const nameValue = nome.value; //adicionei o valor do input a uma variavel
+
+            if(nameValue === ""){ //se vazio
+                errorInput(nome, "Nome é obrigatório.") //chama a classe errorIput e passa dois parametros
+            }else{
+                const formItem = nome.parentElement; // se as duas condiçoes de cima for falsa o usuario preencheu corretamente
+                formItem.className = "input"; 
+            }
+        }
+
+        function checarEmail() { //função para checar o nome se esta vazio
+            const nameValue = emailvalor.value; //adicionei o valor do input a uma variavel
+
+            if(nameValue === ""){ //se vazio
+                errorInput(emailvalor, "Email é obrigatório.") //chama a classe errorIput e passa dois parametros
+            }else{
+                const formItem = emailvalor.parentElement; // se as duas condiçoes de cima for falsa o usuario preencheu corretamente
+                formItem.className = "input"; 
+            }
+        }
+
+        function checarTelefone() { //função para checar o nome se esta vazio
+            const nameValue = telefone.value; //adicionei o valor do input a uma variavel
+
+            if(nameValue === ""){ //se vazio
+                errorInput(telefone, "Telefone é obrigatório.") //chama a classe errorIput e passa dois parametros
+            }else{
+                const formItem = telefone.parentElement; // se as duas condiçoes de cima for falsa o usuario preencheu corretamente
+                formItem.className = "input"; 
+            }
+        }
+
+        function verificarChecagem(){
+            checarNome();
+            checarAtuacao();
+            checarEmail();
+            checarTelefone();
+
+            const formItems = form.querySelectorAll(".input") //adicionei a uma variavel todos os elementos .input
+  
+                const isValid = [...formItems].every( (item) => { //every vai verificar se todas as condições passa em alguma condição q a gente colocar
+                return item.className === "input"; //pro formulario ser enviado precisa estar todos validados com as classe input apenas
+                });
+            
+                if(isValid){ //se estiver validado ira enviar // aqui pode mandar para um banco de dados
+                   alert("CADASTRADO COM SUCESSO!");
+                }
+        }
+        function errorInput (input, msg){ //função para o erro se os campos não estiverem preenchidos vai adicionar uma classe com a msg de erro
+            const item = input.parentElement;  //variavel que vai receber o parenteElement que é o pai do input que seria a class input
+            const msgError = item.querySelector("p"); //variavel recebento o item q vai informar qual parente, e nesse parente da um querySelector para pegar qual elemento filho dele 
+            
+            msgError.innerText = msg; // msgError vai receber a msg que chegara como argumento
+
+            item.className = "input erro"; // e o item vai add um nome de uma classe q é o error onde mostrara o erro na tela
+        }
     </script>
 </body>
 </html>
